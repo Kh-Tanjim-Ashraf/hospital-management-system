b8a2f4a77d69627a4a02f3384cac5dff81a45820
Kh-Tanjim-Ashraf <kh.tanjim.ashraf@gmail.com>
2026-07-19 05:06:34 +0600

    Added the model into the `Appointment` module

--- appointment/models.py
+++ appointment/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from shared.models import TimestampMixins
+from doctor.models import Doctor
 
-# Create your models here.
+
+
+User = get_user_model()
+
+
+
+class Appointment(TimestampMixins):
+
+    STATUS = [
+        ('pend', 'Pending'),
+        ('conf', 'Confirmed'),
+        ('comp', 'Completed'),
+        ('canc', 'Cancelled')
+    ]
+
+    patient_id = models.ForeignKey(User, related_name='patients', on_delete=models.SET_NULL, null=True, blank=True)
+    doctor_id = models.ForeignKey(Doctor, related_name='doctors', on_delete=models.SET_NULL, null=True, blank=True)
+    appointment_date = models.DateField(null=True, blank=True)
+    appointment_time = models.TimeField(null=True, blank=True)
+    status = models.CharField(max_length=4, choices=STATUS, default='pend')
+
+    def __str__(self):
+        return f"Patient: {self.patient_id}; Doctor: {self.doctor_id}; Status: {self.status}"
