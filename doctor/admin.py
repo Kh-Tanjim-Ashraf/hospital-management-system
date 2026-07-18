91cf3c6460c38e5cb50b58135ac98a5ac69d1341
Kh-Tanjim-Ashraf <kh.tanjim.ashraf@gmail.com>
2026-07-19 04:36:45 +0600

    Added the models into the `Doctor` module

--- doctor/admin.py
+++ doctor/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from doctor.models import Doctor, Department, Specialization
 
-# Register your models here.
+
+admin.site.register(Doctor)
+admin.site.register(Department)
+admin.site.register(Specialization)
--- doctor/models.py
+++ doctor/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from shared.models import TimestampMixins
 
-# Create your models here.
+
+User = get_user_model()
+
+
+class Doctor(TimestampMixins):
+
+    user_id = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=150, null=True, blank=True)
+    visiting_fee = models.DecimalField(max_digits=8, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.user_id}: {self.name}"
+
+
+
+class Department(TimestampMixins):
+
+    name = models.CharField(max_length=150)
+    doctors = models.ManyToManyField(Doctor, related_name='departments')
+
+    def __str__(self):
+        return self.name
+
+
+
+class Specialization(TimestampMixins):
+
+    name = models.CharField(max_length=150)
+    doctors = models.ManyToManyField(Doctor, related_name='specializations')
+
+    def __str__(self):
+        return self.name
