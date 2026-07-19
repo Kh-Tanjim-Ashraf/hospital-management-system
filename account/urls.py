f54dc7b200fca07255e609659e938757e22e9cf3
Kh-Tanjim-Ashraf <kh.tanjim.ashraf@gmail.com>
2026-07-19 06:05:08 +0600

    Feat: Implemented API endpoint for refresh token

--- account/urls.py
+++ account/urls.py
@@ -3,9 +3,11 @@ from account.views import (
     PatientRegistration, 
     UserLogin
 )
+from rest_framework_simplejwt.views import TokenRefreshView
 
 
 urlpatterns = [
     path('api/register/', view=PatientRegistration.as_view()),
     path('api/login/', view=UserLogin.as_view()),
+    path('api/token/refresh/', view=TokenRefreshView.as_view()),
 ]
